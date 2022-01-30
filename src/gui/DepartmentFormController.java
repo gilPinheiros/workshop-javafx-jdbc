80c9500ec14d3c2135bca6dcb423a7215e8ce0fd
gilPinheiros <gil.101dalmatas@hotmail.com>
2022-01-29 23:06:00 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
@@ -67,6 +70,9 @@ public class DepartmentFormController implements Initializable{
 	    	notifyDataChangeListeners();
 	    	Utils.currentStage(event).close();
     	}
+    	catch(ValidationException e) {
+    		setErroMessage(e.getErros());
+    	}
     	catch(DbException e) {
     		Alerts.showAlert("Erro saving object", null, e.getMessage(), AlertType.ERROR);
     	}
@@ -83,8 +89,17 @@ public class DepartmentFormController implements Initializable{
 	private Department getFormData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation Erro");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErros("name", "Field can't be empty");
+		}
 		obj.setName(txtName.getText());
+		
+		if(exception.getErros().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -111,6 +126,14 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErroMessage(Map<String, String> erros) {
+		Set<String> fields = erros.keySet();
+		
+		if(fields.contains("name")) {
+			lblErroName.setText(erros.get("name"));
+		}
 	}
 
 }
